44152db61c635073be3b018f3efa09d5f0caf0fa
jmeyers <jmeyers@colum.edu>
2023-09-19 17:24:12 -0500

    code we wrote in class 3

---
+++ InclassFA23/ConsoleAppP2InClass3/ConsoleAppP2InClass3/Basenji.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleAppP2InClass3
+{
+    public class Basenji : Dog
+    {
+        public Basenji() 
+        {
+            this.BarkSound = "Basenjis dont bark they hown and growl";
+        }
+
+        public override string Bark()
+        {
+            //return base.Bark(); //don't to prarent
+            return "Basenjis dont bark they hown and growl";
+        }
+    }
+}
---
+++ InclassFA23/ConsoleAppP2InClass3/ConsoleAppP2InClass3/Mammal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleAppP2InClass3
+{
+    public class Mammal
+    {
+        public bool hasHair;
+        public bool giveBrithToLiveYoung;
+
+        public string Name;
+        public int Weight;
+
+        //Same name as the class and no return type
+        //sets defaults
+        public Mammal()
+        {
+            this.Name = "some mammal maybe a platapus";
+            this.hasHair = true;
+            this.giveBrithToLiveYoung = true;
+            this.Weight = 2;
+        }
+
+        public virtual string About()
+        {
+            return $"Hello I'm a {this}  My name is {this.Name} I weight {this.Weight} ";
+        }
+    }
+}
